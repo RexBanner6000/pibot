81e781365ec2fbcaa831d1814c10fa51abc8cbb0
RexBanner6000 <tghaynes@gmail.com>
2023-09-19 13:26:32 +0100

    Initial commit

---
+++ servos/__main__.py
@@ -0,0 +1,23 @@
+import time
+
+import RPi.GPIO as GPIO
+
+
+def init(servo_pin: int = 1) -> None:
+    GPIO.setup(servo_pin, GPIO.OUT)
+
+
+def servo_pulse(servo_pin: int = 1, angle: float = 5) -> None:
+    pulse_width = angle * 11 + 500
+    GPIO.output(servo_pin, GPIO.HIGH)
+    time.sleep(pulse_width / 1_000_000.0)
+    GPIO.output(servo_pin, GPIO.LOW)
+    time.sleep(20e-3 - pulse_width / 1_000_000.0)
+
+
+if __name__ == "__main__":
+    rotation = 5
+    init(servo_pin=1)
+    print(f"Rotating servo {rotation}")
+    servo_pulse()
+    print("Done!")
