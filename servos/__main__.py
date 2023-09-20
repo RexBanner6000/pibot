806f88e4a5d82a487ed35c01c84baff6f63e368b
RexBanner6000 <tghaynes@gmail.com>
2023-09-20 20:57:47 +0100

    Added motion check function

--- servos/__main__.py
+++ servos/__main__.py
@@ -2,6 +2,7 @@ import time
 from argparse import ArgumentParser
 from typing import Tuple
 
+import numpy as np
 import RPi.GPIO as GPIO
 
 
@@ -31,18 +32,31 @@ class Servo:
         time.sleep(self.dead_space)
         pulse.stop()
 
+    def check_motion(self) -> None:
+        self.send_pulse(self.pulse_range[0])
+        self.send_pulse(self.pulse_range[1])
+        self.send_pulse(self.pulse_range[0])
+        for i in np.arange(self.pulse_range[0], self.pulse_range[1], 0.1):
+            self.send_pulse(i)
+
+        self.send_pulse(np.mean(self.pulse_range))
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-p", "--pin", help="Pin number", type=int, default=5)
     parser.add_argument(
-        "-d", "--duty_cycle", help="Duty cycle", type=float, default=5
+        "-d", "--duty_cycle", help="Duty cycle", type=float, default=None, required=False
     )
 
     args = parser.parse_args()
 
     servo = Servo(args.pin, (3.2, 11.2), (0, 180))
     servo.initialise_pin()
-    servo.send_pulse(args.duty_cycle)
+
+    if args.duty_cycle:
+        servo.send_pulse(args.duty_cycle)
+    else:
+        servo.check_motion()
 
     GPIO.cleanup()
