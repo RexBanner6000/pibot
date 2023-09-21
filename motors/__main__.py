914b1e543d633757c9e5e2fbe731d89b9bb1ea44
RexBanner6000 <tghaynes@gmail.com>
2023-09-21 21:10:15 +0100

    Added cycle_motor function

--- motors/__main__.py
+++ motors/__main__.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 def cycle_motor(motor: Motor) -> None:
-    for speed in [0.1, 0.5, 1]:
+    for speed in [0.5, 1]:
         motor.forward(speed=speed)
         sleep(1)
         motor.backward(speed=speed)
@@ -14,9 +14,9 @@ def cycle_motor(motor: Motor) -> None:
 if __name__ == "__main__":
 
     front_right = Motor(20, 21)
-    front_left = Motor(22, 23)
+    front_left = Motor(26, 27)
     back_left = Motor(24, 25)
-    back_right = Motor(26, 27)
+    back_right = Motor(22, 23)
 
     for motor in [front_right, front_left, back_left, back_right]:
         cycle_motor(motor)
