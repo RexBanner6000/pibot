5f9c065bd0a6e0898df9db265fa36eeeb02ee89b
RexBanner6000 <tghaynes@gmail.com>
2023-09-21 21:07:08 +0100

    Added cycle_motor function

--- motors/__main__.py
+++ motors/__main__.py
@@ -1,9 +1,23 @@
 from gpiozero import Motor
+from time import sleep
+
+
+def cycle_motor(motor: Motor) -> None:
+    for speed in [0.1, 0.5, 1]:
+        motor.forward(speed=speed)
+        sleep(1)
+        motor.backward(speed=speed)
+        sleep(1)
+        motor.stop()
 
 
 if __name__ == "__main__":
-    motor = Motor(20, 21)
-    motor.forward()
-    input("Press enter to stop")
-    motor.stop()
-    print("Stopped")
+
+    front_right = Motor(20, 21)
+    front_left = Motor(22, 23)
+    back_left = Motor(24, 25)
+    back_right = Motor(26, 27)
+
+    for motor in [front_right, front_left, back_left, back_right]:
+        cycle_motor(motor)
+        motor.stop()
