aafe8dbf6180e3367df5be40905e00c5ac0819a4
RexBanner6000 <tghaynes@gmail.com>
2023-09-23 14:50:11 +0100

    Added sweeping servo class

--- servos/__main__.py
+++ servos/__main__.py
@@ -2,6 +2,19 @@ from argparse import ArgumentParser
 from time import sleep
 
 from gpiozero import Servo
+from gpiozero.tools import sin_values
+from signal import pause
+
+
+class SweepingServo(Servo):
+    def __init__(self, pin: int = 5):
+        super(SweepingServo, self).__init__(pin)
+
+    def sweep(self):
+        self.source = sin_values()
+        self.source_delay = 0.1
+
+        pause()
 
 
 if __name__ == "__main__":
@@ -10,10 +23,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    servo = Servo(args.pin)
-    servo.min()
-    sleep(2)
-    servo.mid()
-    sleep(2)
-    servo.max()
-    sleep(2)
+    sweeping_servo = SweepingServo(args.pin)
+    sweeping_servo.sweep()
